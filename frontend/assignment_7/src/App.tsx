d0e442ad30ac37283ae502c3b56d873bc2a35298
irfanMKhan <irfanupdate@yahoo.com>
2024-10-22 09:18:01 -0500

    updated

--- frontend/assignment_7/src/App.tsx
+++ frontend/assignment_7/src/App.tsx
@@ -1,36 +1,22 @@
-
 import "./App.scss";
 import avatar from "./images/bozai.png";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import _ from "lodash";
 import { v4 as uuidv4 } from "uuid";
 import dayjs from "dayjs";
 
-const defaultList = [
-  {
-    rpid: 3,
-    user: { uid: "13258165", avatar: "", uname: "Jay Zhou" },
-    content: "Nice, well done",
-    ctime: "10-18 08:15",
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: { uid: "36080105", avatar: "", uname: "Song Xu" },
-    content: "I search for you thousands of times, from dawn till dusk.",
-    ctime: "11-13 11:29",
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: { uid: "30009257", avatar, uname: "John" },
-    content:
-      "I told my computer I needed a break... now it will not stop sending me vacation ads.",
-    ctime: "10-19 09:00",
-    like: 66,
-  },
-];
+type commentType = {
+  rpid: number;
+  user: {
+    uid: string;
+    avatar: string;
+    uname: string;
+  };
+  content: string;
+  ctime: string;
+  like: number;
+};
 
 const user = {
   uid: "30009257",
@@ -44,7 +30,7 @@ const tabs = [
 ];
 
 const App = () => {
-  const [comments, setComments] = useState(defaultList);
+  const [comments, setComments] = useState<commentType[]>([]);
   const [newComment, setNewComment] = useState("");
 
   const [activeTab, setActiveTab] = useState("hot");
@@ -58,7 +44,7 @@ const App = () => {
       rpid: parseInt(uuidv4()),
       user: user,
       content: newComment,
-      ctime: dayjs().format("MM-DD HH:mm"),
+      ctime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
       like: 0,
     };
 
@@ -66,52 +52,120 @@ const App = () => {
     setNewComment("");
   };
 
-  const sortedComments =
+  useEffect(() => {
+    sortedComments();
+  }, [activeTab]);
+
+  const sortedComments = () =>
     activeTab === "hot"
-      ? _.orderBy(comments, ["like"], ["desc"])
-      : _.orderBy(comments, ["ctime"], ["desc"]);
+      ? setComments([..._.orderBy(comments, ["like"], ["desc"])])
+      : setComments([..._.orderBy(comments, ["ctime"], ["desc"])]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const res = await fetch("http://localhost:3004/comments");
+        const data = await res.json();
+        setComments(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchComments();
+  }, []);
+
+  if (loading) {
+    return <div>Loading comments...</div>;
+  }
 
   return (
     <div className="app">
-
+      {/* Nav Tab */}
       <div className="reply-navigation">
         <ul className="nav-bar">
-          {tabs.map((tab) => (
-            <li className="nav-title" key={tab.type}>
+          <li className="nav-title">
+            <span className="nav-title-text">Comments</span>
+            {/* Like */}
+            <span className="total-reply">{10}</span>
+          </li>
+          <li className="nav-sort">
+            {tabs.map((tab) => (
               <span
-                className={`nav-item ${activeTab === tab.type ? "active" : ""}`}
+                className="nav-item"
+                key={tab.type}
                 onClick={() => setActiveTab(tab.type)}
               >
                 {tab.text}
               </span>
-            </li>
-          ))}
+            ))}
+          </li>
         </ul>
       </div>
 
-      <textarea
-        value={newComment}
-        onChange={(e) => setNewComment(e.target.value)}
-        placeholder="Write your comment..."
-      />
-      <button onClick={postComment}>Post</button>
-
-      {/* Map over the comments array and display them */}
-      {comments.map((comment) => (
-        <div key={comment.rpid} className="reply-item">
-          <div className="user-name">{comment.user.uname}</div>
-          <div className="reply-content">{comment.content}</div>
-          <div className="reply-info">
-            <span className="reply-time">{comment.ctime}</span>
-            <span className="reply-like">Likes: {comment.like}</span>
-            {comment.user.uid === user.uid && (
-              <button onClick={() => deleteComment(comment.rpid)}>
-                Delete
-              </button>
-            )}
+      <div className="reply-wrap">
+        {/* comments */}
+        <div className="box-normal">
+          {/* current logged in user profile */}
+          <div className="reply-box-avatar">
+            <div className="bili-avatar">
+              <img className="bili-avatar-img" src={avatar} alt="Profile" />
+            </div>
+          </div>
+          <div className="reply-box-wrap">
+            {/* comment */}
+            <textarea
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              className="reply-box-textarea"
+              placeholder="tell something..."
+            />
+            {/* post button */}
+            <div className="reply-box-send" onClick={postComment}>
+              <div className="send-text">post</div>
+            </div>
           </div>
         </div>
-      ))}
+        {/* comment list */}
+        <div className="reply-list">
+          {/* comment item */}
+          <div className="reply-item">
+            {/* profile */}
+            <div className="root-reply-avatar">
+              <div className="bili-avatar">
+                <img className="bili-avatar-img" alt="" src={user.avatar} />
+              </div>
+            </div>
+
+            {comments.map((comment) => (
+              <div className="content-wrap">
+                <div className="user-info">
+                  <div className="user-name">{user.uname}</div>
+                </div>
+                <div className="root-reply">
+                  <span className="reply-content">{comment.content}</span>
+                  <div className="reply-info">
+                    {/* comment created time */}
+                    <span className="reply-time">{comment.ctime}</span>
+                    {/* total likes */}
+                    <span className="reply-time">Like:{comment.like}</span>
+                    <span
+                      className="delete-btn"
+                      onClick={() => deleteComment(comment.rpid)}
+                    >
+                      Delete
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
